4f730951fd4322853d4a7a89b8757f12196df53a
joakin <joaquin@chimeces.com>
2023-11-16 02:47:02 +0100

    configure dressing.nvim

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,4 +1,24 @@
 -- Better vim.ui.{select,prompt}
 return {
   "stevearc/dressing.nvim",
+  opts = {
+    input = {
+      default_prompt = "❯ ",
+      mappings = {
+        i = {
+          ["<C-P>"] = "HistoryPrev",
+          ["<C-N>"] = "HistoryNext",
+        },
+      },
+    },
+    select = {
+      backend = { "fzf_lua", "telescope", "fzf", "builtin", "nui" },
+      mappings = {
+        i = {
+          ["<C-P>"] = "HistoryPrev",
+          ["<C-N>"] = "HistoryNext",
+        },
+      },
+    },
+  },
 }
