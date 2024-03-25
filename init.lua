7bc4361836b9832d895543932a3bfb52a3cddcdb
joakin <joaquin@chimeces.com>
2024-03-25 13:00:41 +0100

    comment unused abbrevs

--- init.lua
+++ init.lua
@@ -756,9 +756,9 @@ vim.opt.statusline = statusline()
 -- }}}
 
 -- Abbreviations {{{
-vim.cmd([[iabbrev @@    joaquin@chimeces.com]])
-vim.cmd([[iabbrev wweb  http://chimeces.com]])
-vim.cmd([[iabbrev ssig  <cr>Joaquin Oltra<cr>joaquin@chimeces.com<cr>]])
+-- vim.cmd([[iabbrev @@    joaquin@chimeces.com]])
+-- vim.cmd([[iabbrev wweb  http://chimeces.com]])
+-- vim.cmd([[iabbrev ssig  <cr>Joaquin Oltra<cr>joaquin@chimeces.com<cr>]])
 
 vim.cmd(
   [[iabbrev alice1 Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, `and what is the use of a book,' thought Alice `without pictures or conversation?'<cr>]]
