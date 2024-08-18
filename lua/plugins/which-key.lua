803d4cd9f201143548232c322f3c2e05dfc6e7e9
joakin <joaquin@chimeces.com>
2024-08-18 19:47:17 +0200

    Fix broken which-key, blend option moved

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,13 +3,11 @@ return {
   "folke/which-key.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
   opts = {
     win = {
-      winblend = 3,
+      wo = {
+        winblend = 10,
+      },
     },
   },
 }
